114494ee425b2cce3d3e5a40be027c20d7a32d18
ikamrulashik <mdkamrul.islam@northsouth.edu>
2021-08-24 00:48:36 +0600

    authentication created

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+    <div>
+        <div className="footer">
+            <p>© 2020 Copyright: Kamrul</p>
+        </div>
+    </div>
+  
+);
+
+export default Footer;
--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -2,7 +2,14 @@ import React from 'react'
 import logo from '../logo.svg'
 import doctor from '../images/doctor.png'
 import {Link} from 'react-router-dom'
+import {Col, Container, Row, Form, Button} from "react-bootstrap"
+import Footer from '../Footer'
 
+var icon={
+    width:"300px",
+    height: "70px",
+    marginBotton: "3rem"
+}
 var link={
     color: "blue",
     textDecoration: "none"
@@ -16,35 +23,48 @@ var register={
     margin: "20px",
     textDecoration: "none"
 }
-var form={
-    padding: "50px",
-    background: "#FFFFFF",
-    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.25)",
-}
 
-function Login(){
-    return <div style={{backgroundColor: "F1F8FF"}}>
-       <div className="row mt-5">
-           <div className="col-md-6">
-               <img src={logo} width="200" mt-5 p-5/>
-               <img src={doctor} width="500" mt-10 p-20/>            
-           </div>
-           <div className="col-md-6 my-auto p-5 rounded" style={{float: "left"}}>
-            <form style={form}>            
-                <input type="email" className="form-control" placeholder="Enter address or phone number"/>
-                <br/>
-                <input type="password" className="form-control" placeholder="Password"/>  
-                <br/>
-                <button type="submit" className="btn btn-primary">Log in</button>
-                <br/>
-                <Link style={link} to="/">Forgotten password?</Link>
-                <hr/>
-                <Link style={register} to="/register">Create New Account</Link>
+function Login () {
+    return ( 
+        <div style={{backgroundColor:"#F1F8FF", minHeight: "100vh"}}>
+            <Container className>
+                <Row className="d-flex justify-content-center">
+                    <Col lg={4} md={6} sm={12} className="text-center mt-5 p-3">
+                        <br/>
+                        <img style = {icon} src={logo} alt="icon"/>
+                        <Form>
+                            <Form.Group  className="mb-3" controlId="formBasicEmail">     
+                                <Form.Control type="email" placeholder="Your email" />
+                            </Form.Group>
 
-            </form>
-           </div>
-       </div>
-    </div>
+                            <Form.Group  className="mb-3" controlId="formBasicPassword">
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <div className="d-grid gap-2">
+                                <Button type="submit"variant="primary" size="lg">Log in</Button>
+                            </div>
+                            <br/>
+                            <div className="mt-3">
+                                <Link style={link} to="/">Forgotten password?</Link>
+                                <hr/>
+                                <Link style={register} to="/register">Create New Account</Link>
+                            </div>
+                        </Form>
+
+                    </Col>
+
+                    <Col lg={6} md={6} sm={12}>
+                        <div className="mt-5">
+                            <img className="w-100" src={doctor}/>
+                        </div>
+                        
+                    </Col>
+                </Row>
+            </Container>
+            <Footer/>
+
+        </div>
+    )
 }
 
 export default Login;
