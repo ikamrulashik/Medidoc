8b8620f07224826449834dfe4251bef045157114
ikamrulashik <mdkamrul.islam@northsouth.edu>
2021-11-29 01:20:30 +0600

    changes in footer

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
     <div>
         <div className="footer">
-            <p>© 2021 Copyright: Sameer</p>
+            <p>© 2021 Copyright: MediDoc</p>
         </div>
     </div>
   
